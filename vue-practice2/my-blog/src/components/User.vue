82cd9d73d25d21310a890ea80f6d9e885ad3021f
yushanguanjin <2908635520@qq.com>
2019-03-21 18:02:44 +0800

    wenjianxiugai

--- vue-practice2/my-blog/src/components/User.vue
+++ vue-practice2/my-blog/src/components/User.vue
@@ -1,23 +1,101 @@
 <template>
-    <div>
-        <h1>User</h1>
-        <div>我是user组件, 动态部分是{{dynamicSegment}}</div>
+    <!--表单的增删和修改-->
+    <div class="User">
+      <table>
+        <caption>麻将</caption>
+        <thead>
+        <tr>
+          <th>条</th>
+          <th>筒</th>
+          <th>万</th>
+          <th>操作</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="(mj,index) in mjs" :key="mj">
+          <td>
+            <input type="text" v-model="mj.tiao" v-if="edi===index">
+            <span v-else>{{mj.tiao}}</span>
+          </td>
+          <td>
+            <input type="text" v-model="mj.tong" v-if="edi===index">
+            <span v-else>{{mj.tong}}</span>
+          </td>
+          <td>
+            <input type="text" v-model="mj.wan" v-if="edi===index">
+            <span v-else>{{mj.wan}}</span>
+          </td>
+   
+          <td>
+            <button @click="mod(index)">修改</button>
+            <button @click="save(index)">确定</button>
+            <button @click="del(index)">删除</button>
+          </td>
+        </tr>
+        </tbody>
+        <tfoot>
+          <td colspan="4">
+            <button @click="add">添加</button>
+            <button @click="submit">保存</button>
+          </td>
+        </tfoot>
+      </table>
+   
     </div>
-</template>
-<script>
+  </template>
+   
+  <script>
     export default {
-        data () {
-            return {
-                dynamicSegment: ''
-            }
-        },
-        watch: {
-            $route (to,from){
-                // to表示的是你要去的那个组件，from 表示的是你从哪个组件过来的，它们是两个对象，你可以把它打印出来，它们也有一个param 属性
-                console.log(to);
-                console.log(from);
-                this.dynamicSegment = to.params.id
-            }
+      name: 'User',
+      data () {
+        return {
+          edi:'',
+          mjs: [
+            {tiao:'一条',tong:'一筒',wan:'壹万'},
+            {tiao:'二条',tong:'二筒',wan:'贰万'},
+            {tiao:'三条',tong:'三筒',wan:'叁万'},
+          ]
         }
+      },
+      methods: {
+        mod:function(index){
+          this.edi=index;
+        },
+        del: function (index) {
+          this.mjs.splice(index, 1);
+        },
+        save:function(index){
+          this.edi=!index;
+        },
+        add:function(){
+          this.mjs.push({tiao:'',tong:'',wan:''})
+        },
+        submit:function () {
+          this.edi='';
+          console.log(JSON.stringify(this.mjs))
+        }
+      }
     }
-</script>
+  </script>
+   
+  <style scoped>
+    input{
+      border:none;
+      width:150px;
+    }
+    table{
+    border-collapse: collapse;
+    border:1px solid black
+    }
+    td{
+      text-align: center;
+      width: 150px;
+      height:30px;
+      border:1px solid black
+    }
+    button{
+      border: none;
+      background-color:deepskyblue;
+      color:white;
+    }
+  </style>

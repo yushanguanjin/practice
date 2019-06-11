cf45107be484e54ccecf16e17a44efc7c3f143e5
liupeng <liupeng@refineit.com.cn>
2019-06-11 18:25:41 +0800

    修改

---
+++ xiaochengxu/pages/components/radio/radio.js
@@ -0,0 +1,18 @@
+const app = getApp()
+
+Page({
+  data: {
+    items: [
+      { name: 'USA', value: '美国' },
+      { name: 'CHN', value: '中国', checked: 'true' },
+      { name: 'BRA', value: '巴西' },
+      { name: 'JPN', value: '日本' },
+      { name: 'ENG', value: '英国' },
+      { name: 'FRA', value: '法国' },
+    ]
+  },
+  onLoad: function () {
+    console.log('代码片段是一种迷你、可分享的小程序或小游戏项目，可用于分享小程序和小游戏的开发经验、展示组件和 API 的使用、复现开发问题和 Bug 等。可点击以下链接查看代码片段的详细文档：')
+    console.log('https://mp.weixin.qq.com/debug/wxadoc/dev/devtools/devtools.html')
+  },
+})

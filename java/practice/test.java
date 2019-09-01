357521240bf71e566315325d061946bdf7c5e0d4
yushanguanjin <2908635520@qq.com>
2019-09-01 20:27:37 +0800

    修改

---
+++ java/practice/test.java
@@ -0,0 +1,21 @@
+//public表示公开的
+//class表示定义一个类
+//test表示一个类名
+public class test{//表示定义一个公开的类，起名test
+
+
+    //类体中不允许直接编写java语句(除直接声明外)
+    /*
+    public表示公开的
+    static表示静态的
+    void表示空的
+    main表示方法名是main
+    (String[]   args)是一个main方法的形式参数列表
+
+    
+    以下的方法是一个程序的主方法，是程序的执行入口
+    */
+    public static void main(string[] args) {//表示定义一个公开的静态的主方法
+        System.out.println('test!');
+    }
+}
